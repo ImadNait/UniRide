9a2ca03236524abd7281391182ae60cd64e238b0
krimou <abdelkrim.khateb@gmail.com>
2025-05-01 14:26:32 +0100

    User Class creates along with the constructor and the getters & setters

--- Utilisateur/Utilisateur.java
+++ Utilisateur/Utilisateur.java
@@ -1,25 +1,24 @@
 package Utilisateur;
 public class Utilisateur {
 
-    private final String[] nom ;
-    private final String[] prenom ;
+    private final String nom ;
+    private final String prenom ;
     private final double matricule ;
     private float reputation ;
 // change encapsulation accordingly and without altering the means of security
 
-    Utilisateur(String [] nom, String[] prenom, double matricule) {
+    Utilisateur(String nom, String prenom, double matricule) {
         this.nom = nom;
         this.prenom = prenom;
         this.matricule = matricule;
         this.reputation = 0;
 
     }
-    private String typeUser;
 
-    protected String[] getNom() {
+    protected String getNom() {
         return this.nom;
     }
-    protected String[] getPrenom() {
+    protected String getPrenom() {
         return this.prenom;
     }
     protected double getMatricule() {
