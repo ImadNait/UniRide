2b45a7c133a771b9d6a8b05990ec67d1e99f03b3
krimou <abdelkrim.khateb@gmail.com>
2025-05-01 18:23:32 +0100

    User Class creates along with the constructor and the getters & setters

--- Utilisateur/Utilisateur.java
+++ Utilisateur/Utilisateur.java
@@ -8,42 +8,27 @@ public class Utilisateur {
 // change encapsulation accordingly and without altering the means of security
 
     Utilisateur(String nom, String prenom, double matricule, float rep) {
-        //in main when added to main delete here
-        // if (nom instanceof String && nom.matches("^[a-zA-Z]+$")) { // chooses only the values that contain just letters
         this.nom = nom;
-        // }else{
-        //   System.out.println("Invalid type of input nom has to be a String with only letters");
-        // }
-        // if (prenom instanceof String && prenom.matches("^[a-zA-Z]+$")) { // chooses only the values that contain just letters
         this.prenom = prenom;
-        // }else{
-        //   System.out.println("Invalid type of input prenom has to be a String with only letters");
-        //}
-
         this.matricule = matricule;
-        if (rep >= 0 && rep <= 5) {this.reputation = rep;}
-        else{ System.out.println("Value entered out of range choose a value between 1 and 5");}
-
+        setReputation(rep);
     }
-
-    protected String getNom() {
-        return this.nom;
-    }
-    protected String getPrenom() {
-        return this.prenom;
-    }
-    protected double getMatricule() {
-        return this.matricule;
-    }
-    protected float getReputation() {
-        return this.reputation;
-    }
-
+    protected String getNom() {return this.nom;}
+    protected String getPrenom() {return this.prenom;}
+    protected double getMatricule() {return this.matricule;}
+    protected float getReputation() {return this.reputation;}
     protected void setReputation(float rep) {
-        if (rep >= 0 && rep <= 5) {this.reputation = rep;}
+        if(checkRep(rep)){this.reputation = rep;}
         else{ System.out.println("Value entered out of range choose a value between 1 and 5");}
     }
-
-
-
+    public boolean checkRep(float rep){
+        if (rep >= 0 && rep <= 5) {return true;}else{return false;}
+    }
+    public boolean checkNP(Object temp) {
+// i did object temp to implement oop by checking its type with instanceof it will be dealt with if it causes any problems
+        if (temp instanceof String) {
+            String name = (String) temp;
+            return name.matches("^[\\p{L}]+$");}//check if name is in any language ^[\p{L}]+$ <- this means all letters in all languages upper & lower case
+        return false;
+    }
 }
