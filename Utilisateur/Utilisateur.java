14e11c758fb2f24719359fd46cba45724e72883d
krimou <abdelkrim.khateb@gmail.com>
2025-05-01 17:52:00 +0100

    User Class creates along with the constructor and the getters & setters

--- Utilisateur/Utilisateur.java
+++ Utilisateur/Utilisateur.java
@@ -8,20 +8,20 @@ public class Utilisateur {
 // change encapsulation accordingly and without altering the means of security
 
     Utilisateur(String nom, String prenom, double matricule, float rep) {
-       //in main when added to main delete here
+        //in main when added to main delete here
         // if (nom instanceof String && nom.matches("^[a-zA-Z]+$")) { // chooses only the values that contain just letters
-            this.nom = nom;
+        this.nom = nom;
         // }else{
-         //   System.out.println("Invalid type of input nom has to be a String with only letters");
+        //   System.out.println("Invalid type of input nom has to be a String with only letters");
         // }
-       // if (prenom instanceof String && prenom.matches("^[a-zA-Z]+$")) { // chooses only the values that contain just letters
-            this.prenom = prenom;
+        // if (prenom instanceof String && prenom.matches("^[a-zA-Z]+$")) { // chooses only the values that contain just letters
+        this.prenom = prenom;
         // }else{
-         //   System.out.println("Invalid type of input prenom has to be a String with only letters");
+        //   System.out.println("Invalid type of input prenom has to be a String with only letters");
         //}
 
         this.matricule = matricule;
-        if (rep >= 0 && rep <= 6) {this.reputation = rep;}
+        if (rep >= 0 && rep <= 5) {this.reputation = rep;}
         else{ System.out.println("Value entered out of range choose a value between 1 and 5");}
 
     }
